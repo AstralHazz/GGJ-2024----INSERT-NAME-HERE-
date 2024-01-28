2253f1aa0ef27d0460240d5d56130ea93d3843b4
unknown <vgam@MH301P12.seu.stedwards.edu>
2024-01-28 10:29:05 -0600

    Microwave sfx prefabed

---
+++ Assets/Scripts of Alec's/AudioArray.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioOnCollision : MonoBehaviour
+{
+    // Start is called before the first frame update
+    [SerializeField] private AudioSource myAudioSource;
+    [SerializeField] private AudioClip myAudioClip1;
+    [SerializeField] private AudioClip myAudioClip2;
+    [SerializeField] private float volume = 1.0f;
+    bool objHasRun = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (objHasRun != true)
+        {
+            if (!myAudioSource.isPlaying)
+            {
+                myAudioSource.PlayOneShot(myAudioClip1, volume);
+                objHasRun = true;
+                //PLAYS ONCE
+            }
+            else
+            {
+                myAudioSource.Stop();
+            }
+        }
+    }
+
+}
